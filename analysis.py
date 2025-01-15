25f58c2ad1d20713e21c44e48cd480ded36cdf9d
plo17 <138596581+plo17@users.noreply.github.com>
2025-01-15 09:32:40 +0000

    update

--- analysis.py
+++ analysis.py
@@ -6,15 +6,16 @@ import matplotlib.pyplot as plt
 from statsmodels.stats.contingency_tables import Table2x2
 
 def CHI2(data, group_col, value_col):
-    st.title("Chi-Square Test")
+
+    # Normalizacja danych: zamiana "TC" na "CT"
+    if value_col == "BSM":
+        data[value_col] = data[value_col].replace("TC", "CT")
 
     # Przygotowanie tabeli kontyngencji
-    contingency_table = pd.crosstab(data[group_col], data[value_col])
-    st.write("## Tabela kontyngencji")
-    st.dataframe(contingency_table)
-
+    table = pd.crosstab(data[group_col], data[value_col])
+    
     # Obliczenie Chi-Square (ręczne)
-    observed = contingency_table.values
+    observed = table.values
     row_totals = observed.sum(axis=1)
     col_totals = observed.sum(axis=0)
     total = observed.sum()
@@ -26,20 +27,11 @@ def CHI2(data, group_col, value_col):
     dof = (observed.shape[0] - 1) * (observed.shape[1] - 1)
     p_value = 1 - chi2.cdf(chi2_stat, dof)
 
-    # Wyświetlenie wyników
-    st.write("### Chi-Square Test Results")
-    st.write(f"Chi-Square Statistic: {chi2_stat:.4f}")
-    st.write(f"p-value: {p_value:.4f}")
+    return p_value, chi2_stat, table
 
-    # Interpretacja
-    alpha = 0.05
-    if p_value < alpha:
-        st.write(f"#### The result is statistically significant (Reject H₀). The p-value is {p_value:.4f}, which is less than 0.05.")
-    else:
-        st.write(f"#### The result is not statistically significant (Fail to reject H₀). The p-value is {p_value:.4f}, which is greater than or equal to 0.05.")
-    return p_value, chi2_stat
 
-def HW(data, locus, group):
+
+def HW(data, locus):
 
     # Definiowanie genotypów na podstawie locus
     if locus == "VDR FokI":
@@ -72,31 +64,8 @@ def HW(data, locus, group):
     observed = [aa, ab, bb]
     expected = [expected_aa, expected_ab, expected_bb]
 
-    st.markdown(f"### Wyniki analizy Hardy'ego-Weinberga\ngrupa: {group}, czynnik: {locus}:")
-    st.write(f"**Częstość allelu p i q:** p: {p:.2f}, q: {q:.2f}")
-    st.write(f"**Liczebność obserwowana:** AA: {aa}, AB: {ab}, BB: {bb}")
-    st.write(f"**Liczebność oczekiwana:** AA: {expected_aa:.2f}, AB: {expected_ab:.2f}, BB:{expected_bb:.2f}\n")
-    
-    visualise(genotypes, observed, expected)
-    return p, q, observed, expected
-
-def visualise(genotype, observed, expected):
-
-    # Wykres słupkowy
-    width = 0.35  # Szerokość słupków
-
-    # Tworzenie wykresu
-    fig, ax = plt.subplots(figsize=(8, 5))
-    ax.bar(genotype, observed, width, label='Obserwowane', color='skyblue')
-    ax.bar(genotype, expected, width, bottom=observed, label='Oczekiwane', color='lightgreen')
-
-    # Dodanie tytułu i etykiet osi
-    ax.set_title('Porównanie obserwowanych i oczekiwanych liczby genotypów')
-    ax.set_xlabel('Genotyp')
-    ax.set_ylabel('Liczba')
-    ax.legend()
-
-    st.pyplot(fig)
+    plot = visualise(genotypes, observed, expected)
+    return p, q, observed, expected, plot
 
 
 def OR(data, locus):
@@ -113,24 +82,34 @@ def OR(data, locus):
     B_control = 2 * table.iloc[2, 1] + table.iloc[1, 1]
 
     # Tabela 2x2 dla alleli A i B
-    contingency_table = pd.DataFrame({
+    con_table = pd.DataFrame({
         'Cancer': [A_cancer, B_cancer],
         'Control': [A_control, B_control]
     }, index=['Allele A', 'Allele B'])
 
     # Obliczanie odds ratio
-    OR_value = Table2x2(contingency_table.values).oddsratio
-    OR_confint = Table2x2(contingency_table.values).oddsratio_confint()
+    OR_value = Table2x2(con_table.values).oddsratio
+    OR_confint = Table2x2(con_table.values).oddsratio_confint()
 
-    st.write(f"### Analiza dla locus: {locus}")
-    st.write("#### Tabela 2x2:")
-    st.write(contingency_table)
+    return OR_value, OR_confint, con_table
 
-    # Wyświetlanie wyników
-    st.write(f"Odds Ratio: {OR_value:.2f}")
-    st.write(f"95% Confidence Interval: ({OR_confint[0]:.2f}, {OR_confint[1]:.2f})")
-    
-    return OR_value, OR_confint
+
+def visualise(genotype, observed, expected):
+
+    # Wykres słupkowy
+    width = 0.35  # Szerokość słupków
+
+    # Tworzenie wykresu
+    fig, ax = plt.subplots(figsize=(8, 6))
+    ax.bar(genotype, observed, width, label='Obserwowane', color='skyblue')
+    ax.bar(genotype, expected, width, bottom=observed, label='Oczekiwane', color='lightgreen')
+
+    # Dodanie tytułu i etykiet osi
+    ax.set_title('Porównanie obserwowanych i oczekiwanych liczby genotypów')
+    ax.set_xlabel('Genotyp')
+    ax.set_ylabel('Liczba')
+    ax.legend()
+    return fig
 
 
 def OR_analysis(data):
@@ -149,7 +128,16 @@ def OR_analysis(data):
 
     """
     for locus in ['VDR FokI', 'BSM']:
-        OR_value, OR_confint = OR(data, locus)
+        OR_value, OR_confint, con_table = OR(data, locus)
+
+        st.write(f"### Analiza dla locus: {locus}")
+        st.write("#### Tabela 2x2:")
+        st.write(con_table)
+
+        # Wyświetlanie wyników
+        st.write(f"Odds Ratio: {OR_value:.2f}")
+        st.write(f"95% Confidence Interval: ({OR_confint[0]:.2f}, {OR_confint[1]:.2f})")
+    
     return OR_value, OR_confint
 
 
@@ -169,12 +157,31 @@ def HW_analysis(data):
         częstości alleli (p, q), zaobserwowane liczby genotypów 
         oraz oczekiwane liczby genotypów dla danej grupy i locus.
     """
+    st.markdown(f"### Wyniki analizy Hardy'ego-Weinberga\n\n")
 
+    col1, col2 = st.columns([1,1])
     for locus in ['VDR FokI', 'BSM']:
         for group in ['Cancer', 'Control']:
 
             genotypes = data[data['Grupa'] == group][locus].tolist()
-            p, q, observed, expected = HW(genotypes, locus, group)
+            p, q, observed, expected, plot = HW(genotypes, locus)
+            
+            with col1:
+                st.markdown("  \n")
+                st.markdown(f"  \n**Grupa:** {group}  \n**Czynnik:** {locus}")
+                st.markdown(f"Częstość allelu p i q:\n**p:** {p:.2f}, **q:** {q:.2f}")
+
+                results_df = pd.DataFrame({
+                    "Genotyp": ["AA", "AB", "BB"],
+                    "Obserwowana": observed,
+                    "Oczekiwana": [f"{e:.2f}" for e in expected]
+                })
+                st.dataframe(results_df)
+
+            with col2:  
+                st.pyplot(plot)
+                st.markdown("  \n")
+            
     return p, q, observed, expected
 
 def CHI2_analysis(data):
@@ -187,9 +194,24 @@ def CHI2_analysis(data):
     Zwraca:
         tuple: p-wartość i wartość statystyki Chi-kwadrat.
     """
-
+    st.title("Chi-Square Test")
     # Użytkownik wybiera kolumny do testu
     group_col = st.selectbox("Select the group column", data.columns)
     value_col = st.selectbox("Select the value column", [col for col in data.columns if col != group_col])
-    p_value, chi2_value = CHI2(data, group_col, value_col)
+    p_value, chi2_value, table = CHI2(data, group_col, value_col)
+
+    st.write("#### Tabela kontyngencji")
+    st.dataframe(table)
+    # Wyświetlenie wyników
+    st.write("#### Chi-Square Test Results")
+    st.write(f"**Chi-Square Statistic:** {chi2_value:.4f}")
+    st.write(f"**p-value:** {p_value:.4f}")
+
+    # Interpretacja
+    alpha = 0.05
+    if p_value < alpha:
+        st.write(f"The result is statistically significant (Reject H₀). The p-value is {p_value:.4f}, which is less than 0.05.")
+    else:
+        st.write(f"The result is not statistically significant (Fail to reject H₀). The p-value is {p_value:.4f}, which is greater than or equal to 0.05.")
+    
     return p_value, chi2_value
